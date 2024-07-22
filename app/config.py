f6abc1a5a9d2651f624ba56cc8ad0b36ada0ccfc
Young-hun Lee <73415181+dldudgjs31@users.noreply.github.com>
2024-07-22 09:22:25 +0900

    주석

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 class Config:
     SECRET_KEY = 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = 'mysql://amsdb:ghrn1004!!@my8002.gabiadb.com/amsdb'
+    #변경을 자동으로 추적하고, 변경 사항을 데이터베이스에 반영하는 옵션
     SQLALCHEMY_TRACK_MODIFICATIONS = False
